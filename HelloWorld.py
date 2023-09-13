3f6d77e423c3e23be3881d6242d52bda4b971401
Case4TheWin <96134388+Case4TheWin@users.noreply.github.com>
2023-09-13 12:42:56 -0500

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -13,11 +13,11 @@ def caesar_cipher(text, shift):
         encrypted_text += char
     return encrypted_text
 
-def encrypt_file(input_file, output_file, shift):           #this makes it possible to create a new file with the encrypted text. 
+def encrypt_file(input_file, output_file, shift):                                       #makes it possible to create a new file with the encrypted text
     try:
-        with open(input_file, 'r') as file:           #this opens the inputed file 
-            plain_text= file.read()                   # this reads the file 
-            encrypted_text = caesar_cipher(plain_text, shift)      #this encrypts the file 
+        with open(input_file, 'r') as file:                                             #opens the inputed file
+            plain_text= file.read()                                                     #reads the file
+            encrypted_text = caesar_cipher(plain_text, shift)                           #encrypts the file 
 
         with open(output_file, 'w') as file: 
             file.write(encrypted_text)
@@ -32,15 +32,15 @@ import argparse
 input_file= ""
 output_file= "encrypted_file.txt"
 
-if __name__ == "__main__":
-    parser= argparse.ArgumentParser(description= "Caesar Cipher encryption")
+if __name__ == "__main__":                                                             #prompts the user to enter the file pathway 
+    parser= argparse.ArgumentParser(description= "Caesar Cipher encryption")           #defines the command-line arguments that the script needs 
     parser.add_argument("input_file", help="Path to the file you want to encrypt")
     parser.add_argument("shift", type=int, help="Caesar Cipher shift value")
     
-    file_path= input("Enter file path here:")
-    shift= random.randint(1,25) 
+    file_path= input("Enter file path here:")                                          #asks the user to put in the file path
+    shift= random.randint(1,25)                                                        #makes the shift of the text random 
    
-    encrypt_file(input_file, output_file, shift) 
+    encrypt_file(input_file, output_file, shift)                                      #encrypts the input_file and puts it in the output_file 
     print(f"File '{input_file}' encrypted and saved as '{output_file}'.")
 
 
